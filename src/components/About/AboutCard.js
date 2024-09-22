592e8dd7e4f3bd757b079668018ca718f61c7352
Hamza Younas <deadbody009@gmail.com>
2024-09-22 10:02:42 +0500

    Home and About sections are almost done

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -3,42 +3,43 @@ import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
-  return (
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+	return (
+		<Card className="quote-card-view">
+			<Card.Body>
+				<blockquote className="blockquote mb-0">
+					<p style={{ textAlign: "justify" }}>
+						Hi Everyone, I am <span className="purple">Hamza Younas </span>
+						from <span className="purple"> Lahore, Pakistan.</span>
+						<br />
+						I am currently employed as a software developer at Crystal
+						Solutions.
+						<br />
+						I have completed Bachelors in Computer Science From Minhaj
+						University Lahore.
+						<br />
+						<br />
+						Apart from coding, some other activities that I love to do!
+					</p>
+					<ul>
+						<li className="about-activity">
+							<ImPointRight /> Playing Games
+						</li>
+						<li className="about-activity">
+							<ImPointRight /> Writing Tech Blogs
+						</li>
+						<li className="about-activity">
+							<ImPointRight /> Travelling
+						</li>
+					</ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
-  );
+					<p style={{ color: "rgb(155 126 172)" }}>
+						"Strive to build things that make a difference!"{" "}
+					</p>
+					<footer className="blockquote-footer">Hamza</footer>
+				</blockquote>
+			</Card.Body>
+		</Card>
+	);
 }
 
 export default AboutCard;
